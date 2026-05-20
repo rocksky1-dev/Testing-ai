513d24a9075fb8e04fb5a7ccd07b9967eb866318
rocksky1-dev <npccraft666@gmail.com>
2026-05-20 18:47:12 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        brand: {
+        infinity: {
           primary: '#6366f1',
           secondary: '#a855f7',
-          accent: '#ec4899',
-        },
-      },
+          dark: '#0f172a'
+        }
+      }
     },
   },
   plugins: [],
